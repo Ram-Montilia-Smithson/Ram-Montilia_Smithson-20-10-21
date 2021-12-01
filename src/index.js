dc78ede3bc272e5b18e24317c1fd5afa36563149
Ram Montilia Smithson <ramsmithson1@gmail.com>
2021-12-01 23:17:51 +0200

    Updating index.js
    correcting path import of allReducers

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from "redux"
 import {Provider} from 'react-redux'
-import allReducers from './state management/reducers';
+import allReducers from './state-management/reducers';
 
 const store = createStore(
   allReducers,
